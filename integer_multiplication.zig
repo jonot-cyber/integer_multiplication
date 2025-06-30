90c1c3a2a64af259638061e8ab7c34ddcae23c26
Joseph Tessitore <josephtessitore@protonmail.com>
2025-06-30 11:24:35 -0400

    Add negatives
    yay? the algorithm is still suboptimal lol

--- integer_multiplication.zig
+++ integer_multiplication.zig
@@ -6,6 +6,7 @@ const Cost = struct {
     value: u16 = 0,
     smaller: ?*const Cost = null,
     larger: ?*const Cost = null,
+    negative: bool = false,
     made: std.StaticBitSet(256) = std.StaticBitSet(256).initEmpty(),
 };
 
@@ -34,7 +35,7 @@ fn calcTerm(n: u16) void {
 
         var cost = Cost{
             .n = n,
-            .value = if (larger == smaller) costs[larger].lowest.value + 1 else costs[larger].lowest.value + costs[smaller].lowest.value + 1,
+            .value = costs[larger].lowest.value + costs[smaller].lowest.value + 1,
             .larger = &costs[larger].lowest,
             .smaller = &costs[smaller].lowest,
             .made = costs[larger].lowest.made.unionWith(costs[smaller].lowest.made),
@@ -48,6 +49,37 @@ fn calcTerm(n: u16) void {
                 .larger = &costs[larger].by_term[smaller],
                 .smaller = null,
                 .made = costs[larger].by_term[smaller].made,
+            };
+            shortcut_cost.made.set(n);
+            insertCost(n, shortcut_cost);
+        }
+    }
+}
+
+fn negativePass(n: u16) void {
+    for (2..256) |i| {
+        const larger: i16 = @as(i16, @intCast(i));
+        const smaller: i16 = @as(i16, @intCast(n)) - larger;
+        if (smaller > larger) continue;
+        if (smaller == 0) continue;
+
+        var cost = Cost{
+            .n = n,
+            .value = costs[@intCast(larger)].lowest.value + costs[@abs(smaller)].lowest.value + 1,
+            .negative = smaller < 0,
+            .larger = &costs[@intCast(larger)].lowest,
+            .smaller = &costs[@abs(smaller)].lowest,
+            .made = costs[@intCast(larger)].lowest.made.unionWith(costs[@abs(smaller)].lowest.made),
+        };
+        cost.made.set(n);
+        insertCost(n, cost);
+        if (costs[@intCast(larger)].by_term[@abs(smaller)].n != 0) {
+            var shortcut_cost = Cost{
+                .n = n,
+                .value = costs[@intCast(larger)].by_term[@abs(smaller)].value + 1,
+                .larger = &costs[@intCast(larger)].by_term[@abs(smaller)],
+                .smaller = null,
+                .made = costs[@intCast(larger)].by_term[@abs(smaller)].made,
             };
             shortcut_cost.made.set(n);
             insertCost(n, shortcut_cost);
@@ -96,7 +128,7 @@ fn printPath(cost: *const Cost) !void {
     } else {
         try out.writeByte('?');
     }
-    try out.writeByte('+');
+    try out.writeByte(if (cost.negative) '-' else '+');
     if (cost.smaller) |smaller| {
         try out.print("{d}", .{smaller.n});
     } else {
@@ -109,15 +141,21 @@ fn printPath(cost: *const Cost) !void {
 }
 
 // 2307
+// 2139
+// 2105
 pub fn main() !void {
     buildOne();
     const out = std.io.getStdOut().writer();
     var sum: u16 = 0;
     for (2..256) |i| {
         calcTerm(@intCast(i));
-        sum += costs[i].lowest.value;
-        try out.print("{d}: {d}\n", .{ i, costs[i].lowest.value });
+    }
+    for (0..2) |_| {
+        sum = 0;
+        for (2..256) |i| {
+            negativePass(@intCast(i));
+            sum += costs[i].lowest.value;
+        }
     }
     try out.print("{d}\n", .{sum});
-    try printPath(&costs[7].lowest);
 }
